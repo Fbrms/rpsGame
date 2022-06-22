068ca58a84ebc7019ca2ae3533084370d0797bf8
Fabrizio <fmoatte@gmail.com>
2022-06-21 20:17:34 -0400

    add function that choose a winner in rock paper scissors

--- main.js
+++ main.js
@@ -15,16 +15,40 @@ const randomSelection = () =>{
 
 const playRound = () =>{
     let resultado;
-    let sel = randomSelection();
-    let val1 = prompt("Choose: rock, paper, scissors");
-    while (val1!="rock" && val1!="paper" && val1!="scissors"){
+    let computerSelection = randomSelection();
+    let playerSelection = prompt("Choose: rock, paper, scissors");
+    while (playerSelection!="rock" && playerSelection!="paper" && playerSelection!="scissors"){
         console.log("hubo un error de tipeo, ingrese denuevo su eleccion")
-        val1 = prompt("Choose: rock, paper, scissors");
+        playerSelection = prompt("Choose: rock, paper, scissors");
     }
-    
+    console.log(`La computadora eligio ${computerSelection} y El jugador eligio ${playerSelection}`)
+    if(computerSelection==playerSelection){
+        console.log("hubo un empate")
+        
+    }else if(computerSelection=="rock" && playerSelection =="scissors"){
+        console.log("La computadura gana")
+    }else if(computerSelection=="scissors" && playerSelection =="paper"){
+        console.log("La computadura gana")
+    }else{
+        console.log("El jugador gana");
+    }
 
-    console.log(sel, val1);
 }
 
+const game = () => {
+    let rondas = +prompt("cuantas rondas quiere jugar?");
+    
+    for(let i=0; i<rondas;i++){
+        console.log(`ronda numero ${i+1}`)
+        playRound();
+    }
+    let continuar = prompt("quieres seguir jugando? (s-n)");
+    return continuar;
+    
+}
+let continuar = game();
+if(continuar == s){
+    console.clear();
+    game();
+}
 
-playRound();
